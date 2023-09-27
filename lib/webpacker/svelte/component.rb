b46435669ef527a35a292a29db463748d2637949
Mitchel <mitchel_jager@hotmail.com>
2023-09-27 22:25:31 +0200

    fix: Include CaptureHelper

--- lib/webpacker/svelte/component.rb
+++ lib/webpacker/svelte/component.rb
@@ -3,6 +3,7 @@ module Webpacker
     class Component
       include ActionView::Helpers::TagHelper
       include ActionView::Helpers::TextHelper
+      include ActionView::Helpers::CaptureHelper
 
       attr_accessor :name
 
