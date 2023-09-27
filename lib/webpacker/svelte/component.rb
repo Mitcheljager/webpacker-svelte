201799eca16916481683cc845226b50bd3a556b5
Mitchel <mitchel_jager@hotmail.com>
2023-09-27 22:40:59 +0200

    chore: add actionview::Base

--- lib/webpacker/svelte/component.rb
+++ lib/webpacker/svelte/component.rb
@@ -14,7 +14,7 @@ module Webpacker
       def render(props = {}, options = {}, &block)
         tag = options.delete(:tag) || :div
         data = { data: { "svelte-component" => @name, "svelte-props" => props.to_json } }
-        content = capture(&block) if block_given?
+        content = ::ActionView::Base.capture(&block) if block_given?
 
         content_tag(tag, content, options.deep_merge(data))
       end
